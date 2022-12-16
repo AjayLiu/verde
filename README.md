19d6b926f3e8206ca7a5fca315755e6a3714c5c5
AjayLiu <ajayliu2928@gmail.com>
2022-12-16 01:04:06 -0700

    Fixed AsyncStorage issue

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://blog.logrocket.com/integrating-firebase-authentication-expo-mobile-app/
+
+1. Run `yarn`
+2. Create a `.env` file in the root of the project and fill in information according to `.env.example`
+3. Run `yarn start`
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,6 +2,12 @@
 import { initializeApp } from "firebase/app";
 import Constants from "expo-constants";
 import "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+	initializeAuth,
+	getReactNativePersistence,
+} from "firebase/auth/react-native";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +24,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// initialize auth
+const auth = initializeAuth(app, {
+	persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export { auth };
 export default app;
