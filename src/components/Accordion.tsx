2e158ef3b0b567ee063adc2a6dec1e23e632e176
Nikhil Vyas <nikhilvyas@Nikhils-MacBook-Pro.local>
2023-02-16 17:13:48 -0800

    Accordion Done

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,20 +1,45 @@
-import { useState } from 'react';
-import AccordionItem from "./AccordionItem";
+import React, {useState} from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import { Button } from "react-native-elements";
 
-const Accordion = () => {
-    // Will populate challenges here
+type AccordionProps = {
+    label: string;
+    expanded: string;
+}
+
+const Accordion = (props: AccordionProps) => {
+    const [isExpand, setIsExpand] = useState(true);
     return (
-        <ul className="accordion">
-            {challenges.map((challenges, index) => (
-                <AccordionItem key={index} challenges={challenges} />
-            ))}
+        <View style={styles.container}>
+            <Button
+                buttonStyle = {styles.title}
+                onPress={() => {
+                    setIsExpand(!isExpand);
+                }}
+                title={isExpand ? props.label : props.label + "\n\n" + props.expanded}
+            />
+        </View>
+    )
+}
 
-const Accordion = () => {
-    const [clicked, setClicked] = useState("0");
-
-    return (
-        //...
-    );
-    };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+      },
+      title: {
+        marginTop: 16,
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+        borderWidth: 4,
+        borderColor: '#20232a',
+        borderRadius: 6,
+        backgroundColor: '#61dafb',
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+      },
+});
 
 export default Accordion;
